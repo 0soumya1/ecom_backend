3fbeb6d043b2b70415de64e0c8caff1d1b447943
Soumya <soumyaguptabond@gamil.com>
2023-09-04 19:02:57 +0530

    aa

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose");
 const url = "mongodb+srv://0soumya1:soumya12@ecomm.uvhbi0q.mongodb.net/e-commerce?retryWrites=true&w=majority"
-const url2 = "mongodb://127.0.0.1:27017/e-commerce"
+//const url2 = "mongodb://127.0.0.1:27017/e-commerce"
 mongoose.connect(url);
